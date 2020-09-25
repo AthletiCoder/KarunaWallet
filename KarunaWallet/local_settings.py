bff0b78f2d92288580ac264eebe13ba5f911fb0c
Varun Kumar N <varunkumar.nagandla@gmail.com>
2020-09-25 16:31:35 +0530

    mysql setup done

---
+++ KarunaWallet/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {                                                       
+        'ENGINE': 'django.db.backends.mysql',                         
+        'NAME': 'karunawallet',
+        'USER': 'vedabase',                                   
+        'PASSWORD': 'vedabase',                                        
+        'HOST': 'localhost',
+        'PORT': '3306',                                           
+    }                                                            
+} 
